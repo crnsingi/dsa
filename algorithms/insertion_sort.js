4d98b9ab4a0d456675c7e5482f34edf5786c2910
Cesar <cesarnsingi@hotmail.com>
2024-12-09 12:40:48 +0100

    declare a temporary variable

--- algorithms/insertion_sort.js
+++ algorithms/insertion_sort.js
@@ -1,5 +1,6 @@
 function insertionSort(array) {
     for (let i = 0; i < array.length; i++) { //Loop over array
+        let temp = array[i]; // Declare a temporary variable equal to current item's value
 
     }
 
