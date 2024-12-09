2922eb6999e780f8adfa02f108fc00d4c820c8d3
Cesar <cesarnsingi@hotmail.com>
2024-12-09 12:42:52 +0100

    declare a variable

--- algorithms/insertion_sort.js
+++ algorithms/insertion_sort.js
@@ -1,6 +1,7 @@
 function insertionSort(array) {
     for (let i = 0; i < array.length; i++) { //Loop over array
         let temp = array[i]; // Declare a temporary variable equal to current item's value
+        let j = i -1; //     // Declare a variable equal to one index lower than the current item's index
 
     }
 
