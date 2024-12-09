339eeb0a3c329117df05dec301bff6de10ce1c31
Cesar <cesarnsingi@hotmail.com>
2024-12-09 10:13:46 +0100

    set the current item

--- algorithms/bubble_sort.js
+++ algorithms/bubble_sort.js
@@ -5,8 +5,16 @@
 
         // Loop over array
         for(let i = 0; i < array.length; i++){
+            // If the array has the current value and the next value
+            // and the current value is bigger than the next value:
+            // the two values need to be swapped (assuming you want the items)
+            // to be incrementally sorted).'
+            if(array[i] && array[i + 1] && array[i] > array[i + 1]) {
+                //Set the current item to the next item, and the next item to be current item.
+                [array[i], array[i +1]] = [array[i + 1], array[i]];
+
+            }
 
         }
-
     }
  }
