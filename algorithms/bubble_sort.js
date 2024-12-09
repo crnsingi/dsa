2526db2b0dde626b34a1ac90e917985d1eab5f90
Cesar <cesarnsingi@hotmail.com>
2024-12-09 10:22:07 +0100

    bubble sort

--- algorithms/bubble_sort.js
+++ algorithms/bubble_sort.js
@@ -2,7 +2,6 @@
     let swapped;
     do { 
         swapped = false; // set var swapped to false as default value.
-
         // Loop over array
         for(let i = 0; i < array.length; i++){
             // If the array has the current value and the next value
@@ -10,11 +9,8 @@
             // the two values need to be swapped (assuming you want the items)
             // to be incrementally sorted).'
             if(array[i] && array[i + 1] && array[i] > array[i + 1]) {
-                //Set the current item to the next item, and the next item to be current item.
-                [array[i], array[i + 1]] = [array[i + 1], array[i]];
-                //Set swapped to true, do-while loop will still run
-                swapped = true;
-
+                [array[i], array[i + 1]] = [array[i + 1], array[i]];  //Set the current item to the next item, and the next item to be current item.
+                swapped = true;                                       //Set swapped to true, do-while loop will still run
             }
         }
     } while(swapped);
