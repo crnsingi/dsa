ae84f866edfd722fe3539ab4e259772d4ef50c03
Cesar <cesarnsingi@hotmail.com>
2024-12-09 10:06:33 +0100

    loop over array

--- algorithms/bubble_sort.js
+++ algorithms/bubble_sort.js
@@ -3,5 +3,10 @@
     do { 
         swapped = false; // set var swapped to false as default value.
 
+        // Loop over array
+        for(let i = 0; i < array.length; i++){
+
+        }
+
     }
  }
