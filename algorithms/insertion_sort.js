25ff164be20f3809755edcd1beb0ab7ee202c026
Cesar <cesarnsingi@hotmail.com>
2024-12-09 12:58:54 +0100

    set the previos item equal

--- algorithms/insertion_sort.js
+++ algorithms/insertion_sort.js
@@ -1,7 +1,14 @@
 function insertionSort(array) {
     for (let i = 0; i < array.length; i++) { //Loop over array
         let temp = array[i]; // Declare a temporary variable equal to current item's value
-        let j = i -1; //     // Declare a variable equal to one index lower than the current item's index
+        let j = i - 1;      // Declare a variable equal to one index lower than the current item's index
+
+        // As long as the current item isn't the first item in the array
+        // and the previous item's value is bigger than the current value. You don't override.
+        // set the previous item equal to the current item in the array
+        while (j >=0 && array[j] > temp) {
+            
+        }
 
     }
 
