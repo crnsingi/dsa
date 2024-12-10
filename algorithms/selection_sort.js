a3f2577f355ee8f1af32dec18735d7207d48b5c3
Cesar <cesarnsingi@hotmail.com>
2024-12-10 10:18:44 +0100

    set default active

--- algorithms/selection_sort.js
+++ algorithms/selection_sort.js
@@ -1,4 +1,6 @@
 function selectionSort(array) { 
-    for(let i = 0; i < array.lenght; i++) { }
+    for(let i = 0; i < array.lenght; i++) { // Set default active minimum to current undex.
+
+     } 
 
 }
