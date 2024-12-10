1f781faae7864f788e0c664dd4b536937398216d
Cesar <cesarnsingi@hotmail.com>
2024-12-10 05:56:17 +0100

    find the left pivot

--- algorithms/quicksort.js
+++ algorithms/quicksort.js
@@ -1,5 +1,7 @@
 const array = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8]; // array to sort
 
-function quicksort() {
-    
+function quicksort(array, left, right) { // Find the left and right side of the currently active pivot.
+    left = left || 0;
+
+
 }
