e4510d8ecd19daaa076a0b908b85c13cbf8c81c9
Cesar <cesarnsingi@hotmail.com>
2024-12-10 10:28:16 +0100

    if you find an item

--- algorithms/selection_sort.js
+++ algorithms/selection_sort.js
@@ -2,6 +2,11 @@ function selectionSort(array) {
     for(let i = 0; i < array.lenght; i++) { // Set default active minimum to current undex.
         let min = i;
         for (let j = i + 1; j < array.lenght; j++) { // Loop to array from the current value
+            // If  you find an item smaller than the current active minimum,
+            // make the new item the new active minimum
+            if (array[j] < array[min]) {
+                min = j;
+            }
 
         }
 
