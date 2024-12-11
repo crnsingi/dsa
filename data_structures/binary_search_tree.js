28dff8898a1f587981fde54809dfd1a9eb545220
Cesar <cesarnsingi@hotmail.com>
2024-12-11 12:19:45 +0100

    const self

--- data_structures/binary_search_tree.js
+++ data_structures/binary_search_tree.js
@@ -15,31 +15,32 @@ class BinarySearchTree {
             // If the tree doesn't have a length, set the new node to the root.
             this.root = node;
         } else { 
-            let current = this.root;
+        let current = this.root;
             while (current) {
-                if (node.data < current.data) {
-                 if(!current.left){
-                    // If the current node doesn't have a left value
-                    // and the new node's data is smaller than the current node's data
-                    // set the current node's left value equal to the new node.
-                    current.left = node;
-                    break;
-                 }
-                    // Loop until !current.left returns true.
-                    current = current.left;
-                } else if (node.data > current.data) {
-                    //Same as for previous one, but opposite
-                    if(!current.right) {
-                        current.right = node;
-                        break;
-                    }
-                    current = current.right;
-                } else { 
-                break;
+            if (node.data < current.data) {
+            if(!current.left){
+            // If the current node doesn't have a left value
+            // and the new node's data is smaller than the current node's data
+            // set the current node's left value equal to the new node.
+        current.left = node;
+            break;
                 }
+            // Loop until !current.left returns true.
+                    current = current.left;
+            } else if (node.data > current.data) {
+            //Same as for previous one, but opposite
+            if(!current.right) {
+            current.right = node;
+            break;
+                }
+            current = current.right;
+            } else { 
+            break;
             }
         }
     }
+    }
     remove (data) {
+        const self = this;
 
     }
