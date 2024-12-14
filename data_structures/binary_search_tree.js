bb2171d5a714ce356f8129d0b4cedbd2abd3a5ff
Cesar <cesarnsingi@hotmail.com>
2024-12-14 09:20:12 +0100

    queue push node left

--- data_structures/binary_search_tree.js
+++ data_structures/binary_search_tree.js
@@ -181,7 +181,7 @@ class BinarySearchTree {
                 queue.push(newline);
             }
             if(node.left) { 
-                
+                queue.push(node.left);
             }
 
         }
