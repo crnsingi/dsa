95b5da02406d1e0e149769af7413111c55b14d41
Cesar <cesarnsingi@hotmail.com>
2024-12-23 18:35:57 +0100

    add edgr

--- graph.js
+++ graph.js
@@ -30,6 +30,10 @@ class Graph {
     }
 
     addEdge(vertex1, vertex2) {
+        
+    }
+
+    removeEdge(vertex1, vertex2) {
         // find indeces of verteces.
         const index1 = this.edges[vertex1] ? this.edges[vertex1].indexOf(vertex2) : -1;
         const index2 = this.edges[vertex2] ? this.edges[vertex2].indexOf(vertex1) : -1;
