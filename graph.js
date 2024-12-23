d7a4ce4f5cfebffeb11cd63cf7a7a4e073facf9e
Cesar <cesarnsingi@hotmail.com>
2024-12-23 18:38:11 +0100

    this edges vertex 2 push vertex 1

--- graph.js
+++ graph.js
@@ -32,6 +32,7 @@ class Graph {
     addEdge(vertex1, vertex2) {
         //Add vertives to edges array and increment number of edges.
         this.edges[vertex1].push(vertex2);
+        this.edges[vertex2].push(vertex1);
 
     }
 
