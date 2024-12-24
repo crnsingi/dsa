7ed918b9d1e9ed385c0512b135d7945867d73e1e
Cesar <cesarnsingi@hotmail.com>
2024-12-24 16:27:05 +0100

    if node new line and queue length

--- data_structures/tree.js
+++ data_structures/tree.js
@@ -164,6 +164,9 @@ class Tree {
         while(queue.length) {
             const node = queue.shift();
             string += node.data.toString() + (node.data !== '\n' ? ' ' : "");
+            if(node === newline && queue.length) {
+                
+            }
 
         }
 
