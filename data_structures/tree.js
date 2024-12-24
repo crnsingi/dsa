73f86c60475a063ae97e35a13dedf6db2395cbdb
Cesar <cesarnsingi@hotmail.com>
2024-12-24 09:34:39 +0100

    const parent

--- data_structures/tree.js
+++ data_structures/tree.js
@@ -11,6 +11,8 @@ class Tree {
     add(data, toNodeData) {
         // Create new node /
         const node = new Node(data);
+        // If the toNodeData arg is passed, find that node. Elsem return null.
+        const parent = toNodeData ? this.findBFS(toNodeData) : null;
 
     }
 
