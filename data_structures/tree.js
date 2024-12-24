010fdf91f5bc2511dd990682677772cb73d6761f
Cesar <cesarnsingi@hotmail.com>
2024-12-24 13:22:10 +0100

    this pre order

--- data_structures/tree.js
+++ data_structures/tree.js
@@ -83,7 +83,8 @@ class Tree {
                 fn(node);
             }
             for(const child of node.children) {
-                
+                this.preOrder(child, fn);
+
             }
 
         }
